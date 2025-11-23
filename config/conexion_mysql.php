03aadaa2752d5ffb18b9f4b41ca8bb20d86365f8
AngelLira11 <prodigylira777@gmail.com>
2025-11-22 20:42:22 -0600

    base de datos

--- config/conexion_mysql.php
+++ config/conexion_mysql.php
@@ -2,13 +2,13 @@
 
 // BASE DE DATOS EN SERVIDOR
 
-/*$servername = "sql113.infinityfree.com";
+$servername = "sql113.infinityfree.com";
 $username = "if0_40318520";
 $password = "ctmborsuk1";
-$dbname = "if0_40318520_sistema_venta"; */
+$dbname = "if0_40318520_sistema_venta"; 
 
 // BASE DE DATOS PARA PRUEBAS EN LOCALHOST
-$servername = "localhost";
+/*$servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "sistema_venta"; 
@@ -19,4 +19,4 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 // Verificar la conexión
 if ($conn->connect_error) {
     die("Conexión fallida: " . $conn->connect_error);
-}
+}*/
